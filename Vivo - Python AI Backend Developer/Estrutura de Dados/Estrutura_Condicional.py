2b748c8e3cd2c7bd1cf2cc77c77be45724e98eca
magalps <and.soares@live.com>
2024-12-14 12:36:21 -0300

    Py
    Aulas de Py

---
+++ Vivo - Python AI Backend Developer/Estrutura de Dados/Estrutura_Condicional.py
@@ -0,0 +1,2 @@
+#As estruturas condicionais em Python são usadas para tomar decisões no código com base em condições avaliadas como verdadeiras (True) ou falsas (False). A estrutura principal é o if, que pode ser combinado com elif e else para criar lógicas mais complexas.
+
